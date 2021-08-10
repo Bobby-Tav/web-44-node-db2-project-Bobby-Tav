5b15944a75decd62d988cf09f6eddd5e15a5b528
Bobby <monkeyeden.rjt@gmail.com>
2021-08-10 17:25:09 -0400

    middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,33 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Cars = require('./cars-model')
+const {checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique 
+} = require('./cars-middleware')
+
+const router = express.Router()
+
+router.get('/',async (req,res,next) =>{
+    try{
+       const allCars =  await Cars.getAll()
+       res.json(allCars)
+    }catch(err){
+        next(err)
+    }
+})
+router.get('/:id',checkCarId,async (req,res,next) =>{
+    try{
+        res.json(req.car)
+    }catch(err){
+        next(err)
+    }
+})
+router.post('/',async (req,res,next) =>{
+    res.json('posting a car')
+})
+
+
+
+module.exports = router
